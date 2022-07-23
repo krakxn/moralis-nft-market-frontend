879beee6f5fe5b1add01d2f40e25b06486ff6c26
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 17:52:25 +0530

    Update addEvents.js

--- addEvents.js
+++ addEvents.js
@@ -5,7 +5,6 @@ let chainId = process.env.chainId || 31337
 let moralisChainId = chainId == "31337" ? "1337" : chainId
 const contractAddressArray = contractAddresses[chainId]["NftMarketplace"]
 const contractAddress = contractAddressArray[contractAddressArray.length - 1]
-
 const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL
 const appId = process.env.NEXT_PUBLIC_APP_ID
 const masterKey = process.env.masterKey
@@ -129,17 +128,21 @@ async function main() {
     const listedResponse = await Moralis.Cloud.run("watchContractEvent", itemListedOptions, {
         useMasterKey: true,
     })
+    
     const boughtResponse = await Moralis.Cloud.run("watchContractEvent", itemBoughtOptions, {
         useMasterKey: true,
     })
+    
     const canceledResponse = await Moralis.Cloud.run("watchContractEvent", itemCanceledOptions, {
         useMasterKey: true,
     })
+    
     if (listedResponse.success && canceledResponse.success && boughtResponse.success) {
         console.log("Success! Database Updated with watching events")
     } else {
         console.log("Something went wrong...")
     }
+
 }
 
 main()
